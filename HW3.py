f54b95eadddd3f24ffd7f44af5eedbe48bedbd7a
jbernstein2 <119815139+jbernstein2@users.noreply.github.com>
2025-02-22 19:09:41 -0500

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import make_pipeline
